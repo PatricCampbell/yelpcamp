369e92bf205f39bfb9acdbf6821b8f30608d043b
patric <PatricCampbell@outlook.com>
2017-03-08 18:39:13 -0500

    Add a navbar to the header partial

--- app.js
+++ app.js
@@ -18,7 +18,19 @@ const campGrounds = [
         {
         'name': "Mountain Goat's Rest",
         'image': 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg'
-    }
+    },
+    {
+        'name': 'Salmon Creek',
+        'image': 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'
+    },
+        {
+        'name': 'Bear Mountain',
+        'image': 'https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg'
+    },
+        {
+        'name': 'Granite Hill',
+        'image': 'https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg'
+    },
 ];
 
 app.set('view engine', 'pug');
