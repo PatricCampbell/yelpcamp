3a9b0aac08c3718414f4e6b4bf4486a908022ac5
patric <PatricCampbell@outlook.com>
2017-03-13 20:37:12 -0400

    Add register routes

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-const express     = require('express'),
-      app         = express(),
-      bodyParser  = require('body-parser'),
-      mongoose    = require('mongoose'),
-      Campground  = require('./models/campground'),
-      Comment     = require('./models/comment'),
-      seedDB      = require('./seeds');
+const express       = require('express'),
+      app           = express(),
+      bodyParser    = require('body-parser'),
+      mongoose      = require('mongoose'),
+      passport      = require('passport'),
+      LocalStrategy = require('passport-local')
+      Campground    = require('./models/campground'),
+      Comment       = require('./models/comment'),
+      User          = require('./models/user'),
+      seedDB        = require('./seeds');
 
 mongoose.connect('mongodb://localhost/yelp_camp');
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 seedDB();
+
+// Passport configuration
+app.use(require('express-session')({
+    secret: 'My cat is named Pandora',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Landing page route
 app.get('/', (req, res) => {
@@ -113,6 +128,25 @@ app.post('/campgrounds/:id/comments', (req, res) => {
             })
         }
     })
+});
+// ============
+// Auth Routes
+// ============
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', (req, res) => {
+    User.register(new User({username: req.body.username}), req.body.password, (err, user) => {
+        if (err) {
+            console.log(err);
+            return res.redirect('/register');
+        } else {
+            passport.authenticate('local')(req, res, () => {
+                res.redirect('/campgrounds');
+            });
+        }
+    });
 });
 
 
